24c60aeb709a97637deffa154010eabdd8766190
IdikaEliada <idika1996@gmail.com>
2026-04-13 16:34:33 +0100

    added my fonts to layout

--- layout.tsx
+++ layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 //import { Geist, Geist_Mono } from "next/font/google";
 import  localFont  from "next/font/local";
 import "./globals.css";
+import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
 //const geist = Geist({subsets:['latin'],variable:'--font-sans'});
