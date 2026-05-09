2f40f1bef67835e6d2a148574e865be974811d36
idikaEliada <idika1996@gmail.com>
2026-05-10 00:30:24 +0100

    update sponsors.ts

---
+++ lib/sponsors.ts
@@ -0,0 +1,7 @@
+const logos = [
+  { src: '/GDG-logo.png', alt: 'GDG FUTO Logo' },
+  { src: '/ases_futo_logo.png', alt: 'ASES FUTO Logo' },
+  { src: '/DEFI_FUTO_logo.png', alt: 'Futo Departments Logo' },
+]
+
+export default logos
