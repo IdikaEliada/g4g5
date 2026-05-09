0a8f604eaaaa9e098a80353d7d0005cdc7ab4f34
idikaEliada <idika1996@gmail.com>
2026-05-09 23:33:50 +0100

    update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,9 @@
 export default function Register () {
-  return <div>
-    <h1>Coming soon</h1>
-  </div>
+  return (
+  <>
+    <main className=" w-full py-8 md:py-16 lg:py-32 px-4 md:px-8 lg:px-16">
+      <h1>Under Construction</h1>
+    </main>
+  </>
+)
 }
