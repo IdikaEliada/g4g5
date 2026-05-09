ee18dbc65b8a705d77c209359a360c4360bd4862
idikaEliada <idika1996@gmail.com>
2026-05-09 02:26:28 +0100

    update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -46,7 +46,7 @@ export default function Hero() {
           </div>
 
           <div className="lg:hidden mask-[linear-gradient(to_bottom,#F8F8FF_75,transparent)]">
-            <PixelImage src="/sen-gen.svg" grid="8x8" />
+            <PixelImage src="/sec_gen.png" grid="8x8" />
           </div>
         </div>
         <div className="hidden lg:block h-fit  mask-[linear-gradient(to_bottom,#F8F8FF_80%,transparent, transparent)]">
@@ -54,7 +54,7 @@ export default function Hero() {
 
         </div>
         <div className="absolute hidden lg:block right-0 mask-[linear-gradient(to_bottom,#F8F8FF_60%,transparent)]">
-          <PixelImage src="/sen-gen.svg" grid="8x8" />
+          <PixelImage src="/sec_gen.png" grid="8x8" />
         </div>
 
 
