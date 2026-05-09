f1f42ec549b41c4b3685d063b50155f477cba16a
idikaEliada <idika1996@gmail.com>
2026-05-09 15:13:49 +0100

    update pixel-image.tsx

--- components/ui/pixel-image.tsx
+++ components/ui/pixel-image.tsx
@@ -92,7 +92,7 @@ export const PixelImage = ({
   }, [rows, cols, maxAnimationDelay])
 
   return (
-    <div className="relative h-125 w-125 select-none md:h-175 md:max-w-195 bg-red-800">
+    <div className="relative h-125 w-95 select-none md:h-175 md:max-w-195 bg-red-800">
       {pieces.map((piece, index) => (
         <div
           key={index}
