09ed7f9d4c9c449865cbe2b1e0fad9b2bfc83047
idikaEliada <idika1996@gmail.com>
2026-05-10 00:47:32 +0100

    update sponsors.ts

--- lib/sponsors.ts
+++ lib/sponsors.ts
@@ -6,8 +6,8 @@ interface Sponsors {
 const sponsors: Sponsors[] = [
   { src: 'sponsors/2.jpg', alt: 'GDG FUTO Logo' },
   { src: 'sponsors/e.jpg', alt: 'ASES FUTO Logo' },
-  { src: 'sponsors/DEFI_FUTO_logo.png', alt: 'Futo Departments Logo' },
-  { src: 'sponsors/ases_futo_logo.png', alt: 'ASES FUTO Logo' }, { src: 'sponsors/DEFI_FUTO_logo.png', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/DEFI_FUTO_logo.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/ases_futo_logo.jpg', alt: 'ASES FUTO Logo' }, { src: 'sponsors/DEFI_FUTO_logo.jpg', alt: 'Futo Departments Logo' },
 ]
 
 export default sponsors
