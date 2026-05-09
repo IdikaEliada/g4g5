1682c12773c37c2b5328629c8e09b902f6e06870
idikaEliada <idika1996@gmail.com>
2026-05-10 00:31:16 +0100

    update sponsors.ts

--- lib/sponsors.ts
+++ lib/sponsors.ts
@@ -1,7 +1,12 @@
-const logos = [
+interface Sponsors {
+  src: string
+  alt: string
+}
+
+const sponsors: Sponsors[] = [
   { src: '/GDG-logo.png', alt: 'GDG FUTO Logo' },
   { src: '/ases_futo_logo.png', alt: 'ASES FUTO Logo' },
   { src: '/DEFI_FUTO_logo.png', alt: 'Futo Departments Logo' },
 ]
 
-export default logos
+export default sponsors
