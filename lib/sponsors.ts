7f2cfb144d37486e3658f6a0d3239909f1cac659
idikaEliada <idika1996@gmail.com>
2026-05-10 01:56:00 +0100

    update sponsors.ts

--- lib/sponsors.ts
+++ lib/sponsors.ts
@@ -8,6 +8,19 @@ const sponsors: Sponsors[] = [
   { src: 'sponsors/e.jpg', alt: 'ASES FUTO Logo' },
   { src: 'sponsors/DEFI_FUTO_logo.jpg', alt: 'Futo Departments Logo' },
   { src: 'sponsors/ases_futo_logo.jpg', alt: 'ASES FUTO Logo' }, { src: 'sponsors/DEFI_FUTO_logo.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/s1.jpg', alt: 'GDG FUTO Logo' },
+  { src: 'sponsors/s2.jpg', alt: 'ASES FUTO Logo' },
+  { src: 'sponsors/s3.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/s4.jpg', alt: 'ASES FUTO Logo' }, { src: 'sponsors/s5.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/s5.jpg', alt: 'GDG FUTO Logo' },
+  { src: 'sponsors/s6.jpg', alt: 'ASES FUTO Logo' },
+  { src: 'sponsors/s7.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/s8.jpg', alt: 'ASES FUTO Logo' }, { src: 'sponsors/s5.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/s9.jpg', alt: 'GDG FUTO Logo' },
+  { src: 'sponsors/s10.jpg', alt: 'ASES FUTO Logo' },
+  { src: 'sponsors/s11.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/s12.jpg', alt: 'ASES FUTO Logo' }, { src: 'sponsors/s13.jpg', alt: 'Futo Departments Logo' },
+  { src: 'sponsors/s14.jpg', alt: 'GDG FUTO Logo' },
 ]
 
 export default sponsors
