336b0b8d8150d42f147c66cb331d8d9ff54f6ccd
idikaEliada <idika1996@gmail.com>
2026-05-10 09:05:09 +0100

    update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -68,7 +68,7 @@ export default function Hero() {
                 key={i}
                 src={logo.src}
                 alt={logo.alt}
-                className="h-20 object-contain shrink-0"
+                className="h-16 object-contain shrink-0"
               />))}
           </ScrollVelocityRow>
           <ScrollVelocityRow baseVelocity={10} direction={-1}>
