1d0d3c77d2fd19e3bfefc33ad66215970545e8d1
idikaEliada <idika1996@gmail.com>
2026-05-11 19:05:07 +0100

    update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -133,6 +133,7 @@ export default function NavBar() {
               </Tooltip>
             </DockIcon>
           ))}
+          <Separator orientation="vertical" className="h-full" />
         </Dock>
       </TooltipProvider>
     </div>
