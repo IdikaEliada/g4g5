9389793da72afd055aaca2c9634cd43bd4b0e90f
idikaEliada <idika1996@gmail.com>
2026-05-11 17:58:05 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 //import ShiftingCountdown from '../components/CountDown'
 import Hero from '@/components/Hero'
 import ContentSection from '@/components/ContentSection'
+import NavBar from "@/components/NavBar"  
 
 export default function Home() {
   return (
     <>
       <main className=" w-full py-8 md:py-16 lg:py-32 px-4 md:px-8 lg:px-16">
+        
+        <NavBar />
 
         <section>
           <Hero />
