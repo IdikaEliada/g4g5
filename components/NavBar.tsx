3affa16a27c3032d09cf9ca28b0b878369f06735
idikaEliada <idika1996@gmail.com>
2026-05-11 19:30:27 +0100

    update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -141,7 +141,11 @@ export default function NavBar() {
           <Separator orientation="vertical" className="h-full" />
           {DATA.CTA.map((action) => (
             <button className="text-sm md:text-md  bg-black text-slate-100 p-2 rounded-sm">
-              Register
+              <Link href = { action.href } aria-label = { action.label }
+              >
+                {action.label}
+              </Link>
+              
             </button>
             ))
           }
