0dea51a41a16ecc9474a746d26f6979e76b37afe
idikaEliada <idika1996@gmail.com>
2026-05-11 19:45:53 +0100

    update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -58,7 +58,7 @@ const DATA = {
 
 export default function NavBar() {
   return (
-    <div className="fixed z-200 flex flex-col top-2 right-0 w-full left-0 items-center justify-center">
+    <div className="fixed z-200 flex flex-col top-0 right-0 w-full left-0 items-center justify-center">
       {/*<span className="pointer-events-none bg-linear-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-white dark:to-slate-900/10">
         Dock
       </span>*/}
