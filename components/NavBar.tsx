352d4a3e3b71d46cd4cda7f7e271984da71f1788
idikaEliada <idika1996@gmail.com>
2026-05-11 19:23:13 +0100

    update NavBar.tsx

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -89,7 +89,7 @@ const DATA = {
 
 export default function NavBar() {
   return (
-    <div className="fixed z-200 flex flex-col items-center justify-center">
+    <div className="fixed z-200 flex flex-col w-full items-center justify-center">
       {/*<span className="pointer-events-none bg-linear-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-white dark:to-slate-900/10">
         Dock
       </span>*/}
@@ -140,10 +140,10 @@ export default function NavBar() {
           ))}
           <Separator orientation="vertical" className="h-full" />
           {DATA.CTA.map((action) => (
-            <button className="text-sm bg-black text-slate-100">
+            <button className="text-sm bg-black text-slate-100 p-4">
               Register
             </button>
-          ))
+            ))
           }
         </Dock>
       </TooltipProvider>
