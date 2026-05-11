1df5bd387d67352f22f02663bfdc6d8c300770a3
idikaEliada <idika1996@gmail.com>
2026-05-11 20:26:38 +0100

    update registration-form.tsx

--- components/registration-form.tsx
+++ components/registration-form.tsx
@@ -123,7 +123,7 @@ export default function RegistrationForm() {
           The Quantum Leap
         </h1>
         <span className="pointer-events-none bg-linear-to-b from-black to-gray-300/80 bg-clip-text text-center text-8xl leading-none font-semibold whitespace-pre-wrap text-transparent dark:from-white dark:to-slate-900/10">
-        Dock
+        ₦500K
       </span>
       </div>
 
