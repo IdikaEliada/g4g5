44d5dbe3a7d5b103830af1150ac624fd6cddaff2
idikaEliada <idika1996@gmail.com>
2026-05-11 20:08:04 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,18 @@
 import Hero from '@/components/Hero'
 import ContentSection from '@/components/ContentSection'
 
-
 export default function Home() {
   return (
     <>
-      
-        
-        <section className="mt-8">
-          <Hero />
-        </section>
-        <section>
-          <ContentSection />
-        </section>
+      <section className="mt-8">
+        <Hero />
+      </section>
+      <section>
+        <ContentSection />
+      </section>
         {/* <section>
           <ShiftingCountdown />
         </section> */}
-        
-      
     </>
   )
 }
