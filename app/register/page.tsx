1489437f43189f35a7965d316bb2f45c56394304
idikaEliada <idika1996@gmail.com>
2026-05-11 20:11:37 +0100

    update page.tsx

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,10 +9,10 @@ export const metadata: Metadata = {
 
 export default function RegisterPage() {
   return (
-    <main className=" w-full py-8 md:py-16 lg:py-32 px-4 md:px-8 lg:px-16">
+    <>
       
       <h1>Under Construction</h1>
       <RegistrationForm />
-    </main>
+    </>
   );
 }
