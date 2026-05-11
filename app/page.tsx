a68c3de241066377bc4ed3034cb55459ddeb359a
idikaEliada <idika1996@gmail.com>
2026-05-11 19:39:40 +0100

    update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,10 @@ import NavBar from "@/components/NavBar"
 export default function Home() {
   return (
     <>
+      <NavBar />
       <main className=" w-full py-8 md:py-16 lg:py-32 px-4 md:px-8 lg:px-16">
         
-        <NavBar />
+        
 
         <section className="pt-8">
           <Hero />
